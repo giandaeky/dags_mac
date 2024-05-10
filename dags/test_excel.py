c3b7251dd9163b98778b12bced3fe636a06c5dae
giandaeky <giandaekyoga10@gmail.com>
2024-05-10 11:49:01 +0700

    baru

--- dags/test_excel.py
+++ dags/test_excel.py
@@ -8,9 +8,17 @@ default_args = {
     'start_date': datetime(2024, 5, 10),
 }
 def read_excel_task():
-    excel_file_path = 'https://raw.githubusercontent.com/giandaeky/dags_mac/blob/f28cee5075de42a3b2c184a6dd642ae341eca333/external_code/file_example_XLS_10.xls'
+    excel_file_path = 'https://github.com/giandaeky/dags_mac/blob/2caf89a012fcda22c6b606a24f73c111fbb37194/external_code/file_example_XLSX_10.xlsx'
     df = pd.read_excel(excel_file_path)
     print(df.head()) 
+
+
+# https://github.com/giandaeky/dags_mac/blob/2caf89a012fcda22c6b606a24f73c111fbb37194/external_code/file_example_XLSX_10.xlsx
+# def read_excel_task():
+#     excel_file_path = 'https://raw.githubusercontent.com/giandaeky/dags_mac/7d94db234f60972e50e16e9374ee7cca4fa963e2/external_code/file_example_XLSX_10.xlsx'
+#     df = pd.read_excel(excel_file_path)
+#     print(df.head()) 
+
 
 
 
@@ -25,3 +33,7 @@ with DAG(
         task_id='read_excel_task',
         python_callable=read_excel_task,
     )
+
+
+
+    https://github.com/giandaeky/dags_mac/blob/2caf89a012fcda22c6b606a24f73c111fbb37194/external_code/file_example_XLSX_10.xlsx
