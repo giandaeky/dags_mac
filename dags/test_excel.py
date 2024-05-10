e0215df4ea6ca4d6214d545c0fc9ec84d459f4bc
giandaeky <giandaekyoga10@gmail.com>
2024-05-10 09:29:31 +0700

    ujicoba

---
+++ dags/test_excel.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import pandas as pd
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 5, 10),
+}
+
+def read_excel_task():
+    excel_file_path = '/Users/giandaeky/Gianda/Deployment/airflow/excel/file_example_XLS_10.xls'  
+    df = pd.read_excel(excel_file_path)
+    print(df.head()) 
+
+
+with DAG(
+    'example_dag',
+    default_args=default_args,
+    description='Contoh DAG untuk membaca file Excel dalam Airflow di Kubernetes dengan GitSync',
+    schedule_interval=None,
+) as dag:
+
+    read_excel_operator = PythonOperator(
+        task_id='read_excel_task',
+        python_callable=read_excel_task,
+    )
