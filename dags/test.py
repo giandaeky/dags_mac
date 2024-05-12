d9899a904aa64c2ea8709be945b2ec3b0a69bac8
giandaeky <giandaekyoga10@gmail.com>
2024-05-12 11:17:28 +0700

    test

--- dags/test.py
+++ dags/test.py
@@ -2,21 +2,17 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 
-# Fungsi yang akan dieksekusi oleh operator Python
 def print_hello():
     return 'Hello world!'
 
-# Buat objek DAG dengan nama 'hello_world_dag' dan jadwalnya diatur menjadi sekali sehari
 dag = DAG('hello_world_dag_new', description='Simple DAG for testing Airflow',
           schedule_interval='@daily',
           start_date=datetime(2022, 1, 1), catchup=False)
 
-# Buat operator Python dengan tugas print_hello
 print_hello_task = PythonOperator(
     task_id='print_hello_task1',
     python_callable=print_hello,
     dag=dag,
 )
 
-# Menetapkan dependensi antara tugas
 print_hello_task
