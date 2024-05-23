b14db83cfa063c4d263849bee0ea4aa18612f7f8
giandaeky <giandaekyoga10@gmail.com>
2024-05-23 07:11:41 +0700

    perubahan nama dags

--- dags/test_excel.py
+++ dags/test_excel.py
@@ -29,7 +29,7 @@ def read_excel_task():
 
 
 with DAG(
-    'example_dag12345',
+    'dag excel git (tidak di rekomendasikan)',
     default_args=default_args,
     description='Contoh DAG untuk membaca file Excel dalam Airflow di Kubernetes dengan GitSync',
     schedule_interval=None,
