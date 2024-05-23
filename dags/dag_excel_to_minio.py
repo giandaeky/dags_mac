7149e9fd16301fe3b603b6d7e43012e1bbe9d99d
giandaeky <giandaekyoga10@gmail.com>
2024-05-23 07:40:44 +0700

    perubahan nama dags

--- dags/dag_excel_to_minio.py
+++ dags/dag_excel_to_minio.py
@@ -6,7 +6,8 @@ from airflow.operators.bash_operator import BashOperator
 # def print_hello():
 #     return ' world!'
 
-dag = DAG('hello_world_dag_new', description='Simple DAG for testing Airflow',
+dag = DAG('dag_excel_to_minio', 
+          description='step1',
           schedule_interval='@once',
         #   start_date=datetime(2022, 1, 1), catchup=False
           )
