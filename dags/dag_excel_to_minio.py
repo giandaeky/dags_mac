3f996d48f49a8799c81447bbebfc58c645f07e6b
giandaeky <giandaekyoga10@gmail.com>
2024-05-23 08:54:34 +0700

    modifikasi

--- dags/dag_excel_to_minio.py
+++ dags/dag_excel_to_minio.py
@@ -1,9 +1,26 @@
 from airflow import DAG
 from datetime import datetime
-from airflow.operators.bash_operator import BashOperator
 
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from minio import Minio
+import pandas as pd
+from airflow.hooks.base_hook import BaseHook
 
-
+def read_file_from_minio():
+    # conn = BaseHook.get_connection('minio_default')
+    client = Minio(
+        "gianda-minio.com",
+        access_key="dirumah",
+        secret_key="dirumah123",
+        secure=False  
+    )
+    bucket_name = "bucketrumah"
+    file_name = "file_example_XLSX_10.xlsx"
+    data = client.get_object(bucket_name, file_name)
+    df = pd.read_csv(data)
+    print(df.head())
 
 default_args = {
     'owner': 'airflow',
@@ -21,12 +38,16 @@ dag = DAG(
 )
 
 
-
-run_external_script = BashOperator(
-    task_id='running_script',
-    bash_command='python3 /Users/giandaeky/Gianda/Deployment/projects/minio/send_bucket.py',
+read_task = PythonOperator(
+    task_id='read_file',
+    python_callable=read_file_from_minio,
     dag=dag,
 )
+# run_external_script = BashOperator(
+#     task_id='running_script',
+#     bash_command='python3 /Users/giandaeky/Gianda/Deployment/projects/minio/send_bucket.py',
+#     dag=dag,
+# )
 
 
-run_external_script
+# run_external_script
