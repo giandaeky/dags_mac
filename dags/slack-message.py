b004444481ba95faa569feaf21add729c3599837
giandaeky <giandaekyoga10@gmail.com>
2024-06-01 21:36:28 +0700

    message

---
+++ dags/slack-message.py
@@ -0,0 +1,25 @@
+from airflow import DAG
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': days_ago(1),
+}
+
+dag = DAG(
+    'slack_notification_dag',
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+send_slack_message = SlackAPIPostOperator(
+    task_id='send_message',
+    slack_conn_id='slack_conn',  
+    # token='YOUR_SLACK_OAUTH_TOKEN', 
+    channel='#general', 
+    text='Hello from Airflow!',
+    dag=dag,
+)
+
+send_slack_message
